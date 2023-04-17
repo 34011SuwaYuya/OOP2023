eed59deeca0ca14d322ac821ea6e575e231940f0
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-04-17 10:52:30 +0900

    不要な部分を削除

--- Sample/WindowsFormsApp1/Form1.cs
+++ Sample/WindowsFormsApp1/Form1.cs
@@ -19,7 +19,6 @@ namespace WindowsFormsApp1 {
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            //tbNum1.Text = "楽しいC#！！！";
             BackColor = Color.SpringGreen;
             btButton.BackColor = Color.BurlyWood;
 
