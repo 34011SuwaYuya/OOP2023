59ec5cb51e5de49d72f5291b663ebd5b68a8db89
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-09 11:46:02 +0900

    計算ロジックをメソッドとして独立させる

---
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DistanceConverter {
+    class Program {
+        static void Main(string[] args) {
+            //フィートからメートルへの対応表を出力
+            for (int feet = 1 ; feet <= 10; feet++)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0}ft = {1:0.0}m", feet, meter);
+            }
+
+        }
+
+        //フィートからメートルを求める　繰返し利用するために
+        static double FeetToMeter(int feet) {      //シグネチャ
+            return feet * 0.3048;
+        }
+    }
+}
