eb80994bcdccb1e0097926b268e2db9cc6c0215e
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-09 09:42:17 +0900

    0509演習3

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -12,31 +12,31 @@ namespace ProductSample {
 
         static void Main(string[] args) {
 
-           
-        #region P26            
-        /*
-        //インスタンスの生成（オブジェクトの生成）
-        Product karinto = new Product(123, "かりんとう", 180);
-        Product daifuku = new Product(235, "大福もち", 160);
 
-        Console.WriteLine("かりんとうの税込価格" + karinto.GetPriceIncludingTax());
-        Console.WriteLine( karinto.ToString()) ;
+            #region P26            
+            /*
+            //インスタンスの生成（オブジェクトの生成）
+            Product karinto = new Product(123, "かりんとう", 180);
+            Product daifuku = new Product(235, "大福もち", 160);
 
-        Console.WriteLine("大福もちの税込価格" + daifuku.GetPriceIncludingTax());
-        Console.WriteLine(daifuku.ToString());
-        */
-        #endregion
+            Console.WriteLine("かりんとうの税込価格" + karinto.GetPriceIncludingTax());
+            Console.WriteLine( karinto.ToString()) ;
 
-        #region 日付計算
-        DateTime date = DateTime.Today;
+            Console.WriteLine("大福もちの税込価格" + daifuku.GetPriceIncludingTax());
+            Console.WriteLine(daifuku.ToString());
+            */
+            #endregion
+
+            #region 日付計算
+            DateTime date = DateTime.Today;
             DateTime dayAfter10 = date.AddDays(10);
             DateTime dayBefore10 = date.AddDays(-10);
 
-            
+
             Console.WriteLine("今日の日付：" + date.Year + "年" + date.Month + "月" + date.Day + "日");
             Console.WriteLine("１０日後：" + dayAfter10.Year + "年" + dayAfter10.Month + "月" + dayAfter10.Day + "日");
             Console.WriteLine("１０日前：" + dayBefore10.Year + "年" + dayBefore10.Month + "月" + dayBefore10.Day + "日");
-        #endregion
+            #endregion
 
 
             int year;
@@ -57,10 +57,45 @@ namespace ProductSample {
             DateTime birthDay = new DateTime(year, month, day);
             DateTime today = DateTime.Today;
 
+            DayOfWeek dayOfWeek = birthDay.DayOfWeek;
+            string str = "〇〇";
+            str = NewMethod(dayOfWeek);
+
             TimeSpan interval = today - birthDay;
 
             Console.WriteLine("あなたは生まれてから今日まで" + interval.Days + "日目です");
+            Console.WriteLine("あなたは" + str + "に生まれました。");
 
+        }
+
+        private static string NewMethod(DayOfWeek dayOfWeek) {
+            string str = "";
+            switch ((int)dayOfWeek)
+            {
+                case 0:
+                    str = "日曜日";
+                    break;
+                case 1:
+                    str = "月曜日";
+                    break;
+                case 2:
+                    str = "火曜日";
+                    break;
+                case 3:
+                    str = "水曜日";
+                    break;
+                case 4:
+                    str = "木曜日";
+                    break;
+                case 5:
+                    str = "金曜日";
+                    break;
+                case 6:
+                    str = "土曜日";
+                    break;
+            }
+
+            return str;
         }
     }
 }
