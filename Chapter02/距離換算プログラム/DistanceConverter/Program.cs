6f2ceadc0cf7ebb278aba5f8202dc63ac392848a
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-10 10:11:18 +0900

    修正

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -7,23 +7,29 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
-            if(args.Length >= 1 && args[0] == "-tom")
-            {
-                //フィートからメートルへの対応表を出力
-                for (int feet = 1; feet <= 10; feet++)
-                {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
-                }
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeter(1, 10);
             }
-            else
-            {
-                //メートルからフィートへの対応表を出力
-                for (int meter = 1; meter <= 10; meter++)
-                {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
-                }
+            else {
+                PrintMeterToFeet(1, 10);
+            }
+        }
+
+        private static void PrintMeterToFeet(int start, int stop) {
+
+            //メートルからフィートへの対応表を出力
+            for (int meter = 1; meter <= 10; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
+            }
+        }
+
+        private static void PrintFeetToMeter(int start , int stop) {
+
+            //フィートからメートルへの対応表を出力
+            for (int feet = 1; feet <= 10; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000}m", feet, meter);
             }
         }
 
