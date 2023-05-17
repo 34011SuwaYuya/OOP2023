91509e010d344acf932b4af49f947a5d8334ba62
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-17 12:09:54 +0900

    inch変換の改良

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    //フィートとメートルの単位変換クラス
+    public static class InchConverter {
+        private const double ratio = 0.0254;
+
+        
+
+        //インチからメートルを求める
+        public static double InchToMeter(double inch) {
+            return ratio * inch;
+        }
+
+        //メートルからインチを求める
+        public static double MeterToInch(double meter) {
+            return meter / ratio;
+        }
+
+
+
+    }
+}
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -2,11 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+
+                if(args.Length >= 3 ) {
+                    InchToMeter(int.Parse(args[1]), int.Parse(args[2]));
+                    
+                }else {
+                    InchToMeter(1, 10);
+                }
+                
+            }
+            else if (args.Length >= 1 && args[0] == "-toi") {
+                if (args.Length >= 3) {
+                    MeterToInch(int.Parse(args[1]), int.Parse(args[2]));
+                }else {
+                    MeterToInch(1, 10);
+                }
+            }
+            else {
+
+            }
+        }
+
+        private static void InchToMeter(int start, int stop) {
+
+            //フィートからメートルへの対応表を出力
+            for (int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.InchToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000}m", inch, meter);
+            }
+        }
+
+        private static void MeterToInch(int start, int stop) {
+
+            //メートルからフィートへの対応表を出力
+
+            for (int meter = start; meter <= stop; meter++) {
+                double inch = InchConverter.MeterToInch(meter);
+                Console.WriteLine("{0} m = {1:0.0000}inch", meter, inch);
+            }
         }
     }
+    
 }
