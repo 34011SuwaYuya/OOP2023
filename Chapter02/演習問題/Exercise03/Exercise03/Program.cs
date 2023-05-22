b9053c10b27655a9d18e700ebf615c04a31bc7a7
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-22 10:19:09 +0900

    問題2.3　I/F変更

--- Chapter02/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Exercise03/Program.cs
@@ -11,11 +11,16 @@ namespace Exercise03 {
             SalesCounter sales = new SalesCounter(@"data\Sales.csv");
 
             IDictionary<string, int> salesDictionary;
-            if (args[0] == "product") {
-                salesDictionary = sales.GetPerProductCategory();
-            }
-            else if (args[0] == "store"){
+            Console.WriteLine("1:店舗別売り上げ");
+            Console.WriteLine("2:商品カテゴリー別売り上げ");
+
+            //分岐はcaseが望ましい
+            if (args[0] == "1") {
+                
                 salesDictionary = sales.GetPerStoreSales();
+            }
+            else if (args[0] == "2"){
+                salesDictionary = sales.GetPerProductCategory();
             }
             else {
                 salesDictionary = null;
