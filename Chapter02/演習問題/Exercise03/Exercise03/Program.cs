2c2f1034d73e348fd822f69f424c34018653636b
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-22 10:01:58 +0900

    問題2.3商品カテゴリ別のみ

---
+++ Chapter02/演習問題/Exercise03/Exercise03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class Program {
+        static void Main(string[] args) {
+
+            SalesCounter sales = new SalesCounter(@"data\Sales.csv");
+
+
+            IDictionary<string, int> amountPerProduct = sales.GetPerProductCategory();
+
+            foreach (KeyValuePair<string, int> obj in amountPerProduct) {
+                Console.WriteLine("{0} {1:#,000}", obj.Key, obj.Value);
+            }
+        }
+    }
+}
---
+++ Chapter03＿予習/lambda/typeLambda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace typeLambda {
+    class Program {
+        static void Main(string[] args) {
+
+            var list = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong"};
+
+
+            var exists = list.Exists(s => s[0] == 'A');
+            Console.WriteLine(exists);
+
+
+            var name = list.Find(s => s.Length == 6);
+            Console.WriteLine(name);
+
+
+            int index = list.Find
+            Console.WriteLine();
+            
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+    }
+}
