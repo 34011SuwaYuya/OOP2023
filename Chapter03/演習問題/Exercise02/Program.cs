305a30dbd5778f4ef54b6b61f7fac362f0e74abb
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-05-24 11:33:45 +0900

    演習問題3.2完成

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -18,33 +18,61 @@ namespace Exercise02 {
                 "Hong Kong",
             };
 
-            //3.2-1
-            var line = Console.ReadLine();
-            int ans = names.FindIndex(s => s == line);
-            Console.WriteLine(ans);
-            Console.WriteLine("------");
+            Exercise2_1(names);
+            Console.WriteLine();
+            Exercise2_2(names);
+            Console.WriteLine();
+            Exercise2_3(names);
+            Console.WriteLine();
+            Exercise2_4(names);
 
+        }
+
+        private static void Exercise2_1(List<string> names) {
+
+            //3.2-1
+            var line = "";
+
+
+            Console.WriteLine("***** 3.1 *****");
+            Console.WriteLine("都市名を入力。空行で終了");
+
+            do {
+                line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line)) {
+                    break;
+                }
+                int ans = names.FindIndex(s => s == line);
+                Console.WriteLine(ans);
+            } while (true);
+          
+        }
+
+        private static void Exercise2_2(List<string> names) {
             //3.2-2
+
+            Console.WriteLine("***** 3.2 *****");
             int oCount = names.Count(s => s.Contains('o'));
             Console.WriteLine(oCount);
-            Console.WriteLine("------");
 
+        }
+
+        private static void Exercise2_3(List<string> names) {
 
             //3.2-3
+            Console.WriteLine("***** 3.3 *****");
             var oNames = names.Where(s => s.Contains('o')).ToArray();
             foreach (var s in oNames) {
                 Console.WriteLine(s);
-               
             }
-            Console.WriteLine("------");
+        }
 
-            //3.2-4
-            var namesStartB = names.Where(s => s.StartsWith("B")).Select(n => n.Length);
+        private static void Exercise2_4(List<string> names) {
+            Console.WriteLine("***** 3.4 *****");
+            var namesStartB = names.Where(s => s.StartsWith("B")).Select(s => s + "," + s.Length);
             foreach (var nu in namesStartB) {
                 Console.WriteLine(nu);
             }
-            Console.WriteLine("------");
-
         }
     }
 }
