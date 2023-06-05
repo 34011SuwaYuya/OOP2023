c192f57e85f16d7f43215a9b14ca7bc02079370f
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-06-05 10:03:49 +0900

    問題5.3完成

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -2,11 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+            
+        }
+
+        private static void Exercise3_1(string text) {
+            int blankNum = text.Count(c => c == ' ');
+            Console.WriteLine(blankNum);
+        }
+
+        private static void Exercise3_2(string text) {
+
+            var newText = text.Replace("big", "small");
+            Console.WriteLine(newText);
+        }
+
+        private static void Exercise3_3(string text) {
+            var words = text.Split(' ');
+            Console.WriteLine(words.Length);
+        }
+
+        private static void Exercise3_4(string text) {
+            var words = text.Split(' ').Where(s => s.Length <= 4);
+            foreach (var item in words) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Exercise3_5(string text) {
+            var words = text.Split(' ');
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in words) {
+                sb.Append(item + " ");
+            }
+            Console.WriteLine(sb.ToString().TrimEnd());
         }
     }
 }
