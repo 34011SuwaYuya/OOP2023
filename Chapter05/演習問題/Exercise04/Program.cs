a4b16760d83368373e38803ce49c545e1d071418
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-06-06 11:15:16 +0900

    5.4解説後

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,7 +1,8 @@
 ﻿//#define NonArray
-#define StringArray
+//#define StringArray
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,20 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
 #if NonArray
 
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
             printBook(line);
 
+            /*
+            var words = line.Split(';');
+            foreach (var item in words) {
+                var pair = item.Split('=');
+                Console.WriteLine("{0} : {1}", ToJapanese( pair[0]), pair[1]);
+            }
+            */
 
 #elif StringArray
             var lines = new string[] {
@@ -21,26 +31,60 @@ namespace Exercise04 {
                 "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1887",
                 "Novelist=太宰治;BestWork=人間失格;Born=1909",
                 "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1887",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1887",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
             };
             printBooksForArray(lines);
+            /*
+            foreach (var line in lines) {
+                var words = line.Split(';');
+                foreach (var item in words) {
+                    var pair = item.Split('=');
+                    Console.WriteLine("{0} : {1}", ToJapanese(pair[0]), pair[1]);
+                }
+            }
+            */
+
 
 #endif
+            Console.WriteLine("実行時間　＝　{0}", sw.Elapsed.ToString(@"ss\.ffff"));
+        }
 
+        private static string ToJapanese(string key) {
+            switch (key) {
+                case "Novelist":
+                    return "作家";
+                case "BestWork":
+                    return "代表作";
+                case "Born":
+                    return "誕生年";
+                default:
+                    return "引数エラー";
+            }
         }
 
         private static string[] lineDecompose(string line) {
             
             var words = line.Split(';');
+
             int equalPoint;
             for (int i = 0; i < words.Length; i++) {
-                equalPoint = words[i].IndexOf("=");
+                equalPoint = words[i].IndexOf('=');
                 words[i] = words[i].Remove(0, equalPoint + 1);
 
             }
 
             return words;
         }
-
+#if NonArray 
         //１冊の本用のメソッド
         private static void printBook(string line) {
             
@@ -55,6 +99,8 @@ namespace Exercise04 {
                 Console.WriteLine(formatWords[i] + words[i]);
             }
         }
+#endif
+#if StringArray
 
         //複数の本用のメソッド
         private static void printBooksForArray(string[] lines) {
@@ -74,5 +120,6 @@ namespace Exercise04 {
                 Console.WriteLine("----------------");
             }
         }
+#endif
     }
 }
