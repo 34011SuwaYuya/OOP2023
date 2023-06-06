f87b68c04b47392a293c20750461f846b72d8788
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-06-06 10:04:33 +0900

    一冊用、複数冊用にメソッドの変更

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define NonArray
+#define StringArray
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,26 +9,70 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
-            var originalString = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var words = originalString.Split(';');
+#if NonArray
+
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            printBook(line);
+
+
+#elif StringArray
+            var lines = new string[] {
+                "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886",
+                "Novelist=夏目漱石;BestWork=坊ちゃん;Born=1887",
+                "Novelist=太宰治;BestWork=人間失格;Born=1909",
+                "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
+            };
+            printBooksForArray(lines);
+
+#endif
+
+        }
+
+        private static string[] lineDecompose(string line) {
+            
+            var words = line.Split(';');
             int equalPoint;
             for (int i = 0; i < words.Length; i++) {
                 equalPoint = words[i].IndexOf("=");
-                words[i] = words[i].Remove(0 , equalPoint + 1);
+                words[i] = words[i].Remove(0, equalPoint + 1);
+
             }
 
-            foreach (var item in words) {
-                Console.WriteLine(item);
+            return words;
+        }
+
+        //１冊の本用のメソッド
+        private static void printBook(string line) {
+            
+            var formatWords = new String[]{
+                "作家　：",
+                "代表作：",
+                "誕生年：",
+            };
+
+            var words = lineDecompose(line);
+            for (int i = 0; i < words.Length; i++) {
+                Console.WriteLine(formatWords[i] + words[i]);
             }
+        }
+
+        //複数の本用のメソッド
+        private static void printBooksForArray(string[] lines) {
 
             var formatWords = new String[]{
                 "作家　：",
                 "代表作：",
                 "誕生年：",
             };
-            for (int i = 0; i < words.Length; i++) {
-                Console.WriteLine(formatWords[i] + words[i]);
+            var words = new string[3];
+
+            for (int i = 0; i < lines.Length; i++) {
+                words = lineDecompose(lines[i]);
+                for (int j = 0; j < words.Length; j++) {
+                    Console.WriteLine(formatWords[j] + words[j]);
+                }
+                Console.WriteLine("----------------");
             }
-         }
+        }
     }
 }
