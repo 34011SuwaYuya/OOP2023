fcb648f3d4d016c0ae7cca170d9be81ae34494f1
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-06-19 14:17:39 +0900

    Exercise02一部訂正

--- chapter06/演習問題/Exercise02/Program.cs
+++ chapter06/演習問題/Exercise02/Program.cs
@@ -8,16 +8,21 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
+            #region Books格納
+
             var books = new List<Book> {
                new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
                new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
                new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
-               new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+               new Book { Title = "ワンダフル・C#ライフ", Price = 4800, Pages = 464 },
                new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
                new Book { Title = "私でも分かったASP.NET MVC", Price = 3200, Pages = 453 },
                new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
             };
 
+            #endregion  
+
+            #region 呼び出し部分
             Exercise2_1(books);
             Console.WriteLine("-----");
 
@@ -41,6 +46,7 @@ namespace Exercise02 {
             Exercise2_7(books);
             Console.WriteLine("-----");
             Console.WriteLine();
+            #endregion  
         }
 
         private static void Exercise2_1(List<Book> books) {
@@ -48,7 +54,7 @@ namespace Exercise02 {
             //where句はIEnumerableをリターンする
             var theBooks = books.Where(x => x.Title == "ワンダフル・C#ライフ");
             foreach (var item in theBooks) {
-                Console.WriteLine($"{item.Title}  :  {item.Pages}");
+                Console.WriteLine($"{item.Title}  : \\{item.Price} : {item.Pages}");
             }
 
         }
