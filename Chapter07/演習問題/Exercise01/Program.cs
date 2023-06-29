ce6e0ec7f2aefbd0fe0b1766cfc41e1af422490f
諏訪 佑哉 <OIBC34011@ojs.ac.jp>
2023-06-29 12:01:33 +0900

    Exercise02 7.2.3まで実装

--- Chapter07/演習問題/Exercise01/Program.cs
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -2,32 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
     class Program {
+        static Dictionary<char, int> _dict = new Dictionary<char, int>();
+
         static void Main(string[] args) {
-            string original = "Cozy lummox gives smart squid who asks for job pen";
             
+            string text = "Cozy lummox gives smart squid who asks for job pen".ToUpper();
 
-            Dictionary<char, int> abcDictionary = new Dictionary<char, int>();
-            foreach (var item in Enumerable.Range('A', 26).Select(i => (char)i)) {
-                abcDictionary.Add(item, 0);
+            Exercise1_1(text);
+            Console.WriteLine("**************:");
+            Exercise1_2(text);
+
+
+        }
+
+        private static void Exercise1_1(string text) {
+            
+            foreach (var ch in text) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (_dict.ContainsKey(ch)) {
+                        _dict[ch]++;
+                    }
+                    else {
+                        _dict.Add(ch, 1);
+                    }
+                }
             }
-
-
-
-            char c;
-            for (int i = 0 ; i < original.Length ; i++) {
-                c = original[i];
-                //abcDictionary[original[i]]++;
-                Console.Write(c);
+            foreach (var keyValuePair in _dict.OrderBy(x => x.Key)) {
+                Console.WriteLine("{0}:{1}", keyValuePair.Key, keyValuePair.Value);
             }
+        }
 
-            //foreach (var item in abcDictionary) {
-            //    Console.WriteLine("{0}:{1}", abcDictionary.Keys, abcDictionary.Values);
-            //}
+        private static void Exercise1_2(string text) {
+            SortedDictionary<char, int> sortedPairs = new SortedDictionary<char, int>();
+            foreach (var ch in text) {
 
+                if ('A' <= ch && ch <= 'Z') {
+                    if (sortedPairs.ContainsKey(ch)) {
+                        sortedPairs[ch]++;
+                    }
+                    else {
+                        sortedPairs.Add(ch, 1);
+                    }
+                }
+            }
+            foreach (var item in sortedPairs) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
         }
     }
 }
---
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var abbr = new Abbreviations();
+            Console.WriteLine(abbr.Count());
+            Console.WriteLine(abbr.Remove("APEC"));
+
+            Console.WriteLine(abbr.Count());
+
+        }
+    }
+}
