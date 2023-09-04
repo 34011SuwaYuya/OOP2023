ee0ba1f20c091c9f7e84d4828c085aae0ddc8c63
諏訪佑哉 <OIBC34011@ojs.ac.jp>
2023-09-04 15:58:41 +0900

    問題12.2回答途中

---
+++ Chapter12/演習問題/Exercise02/Novelist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Exercise02 {
+    [XmlRoot("novleist")]
+    class Novelist {
+        [XmlElement(ElementName  = "name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "birth")]
+        public string Birth { get; set; }
+
+        [XmlElement(ElementName = "masterpieces")]
+        public string[] Masterpieces { get; set; }
+
+        
+
+        public override string ToString() {
+            return Name + "  " + Birth + "  ";
+        }
+    }
+
+}
--- Chapter12/演習問題/Exercise02/Program.cs
+++ Chapter12/演習問題/Exercise02/Program.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Runtime.Serialization;
 
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
+            using (var reader = XmlReader.Create ( "Sample.xml" )) {
+                var serializer = new DataContractSerializer ( typeof(Novelist) );
+                Novelist novelist = serializer.ReadObject ( reader ) as Novelist;
 
+                Console.WriteLine ( novelist );
+            }
         }
     }
 }
+
