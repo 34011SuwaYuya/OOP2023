5ec0aeae6130af42bb65437d00f861dba9316c11
諏訪佑哉 <OIBC34011@ojs.ac.jp>
2023-09-12 15:57:54 +0900

    09月12日終了時点

---
+++ Chapter13/SampleEntityFramework/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleEntityFramework.Models {
+    public class Book {
+
+        public int Id{ get; set; }
+        public string Title{ get; set; }
+        public int PublishedYear{ get; set; }
+            //dgvを書き換えるとcarReportsに反映される
+        public virtual Author Author { get; set; }
+    }
+}
