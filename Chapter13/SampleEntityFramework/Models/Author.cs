c4cc0ce90296d52367e71eed0ba577ed2e87c65b
諏訪佑哉 <OIBC34011@ojs.ac.jp>
2023-09-13 11:09:44 +0900

    データ注釈

--- Chapter13/SampleEntityFramework/Models/Author.cs
+++ Chapter13/SampleEntityFramework/Models/Author.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SampleEntityFramework.Models {
+
+        
         public class Author {
         public int Id{ get; set; }
+        [MaxLength(30)]
+        [Required]
         public string Name{ get; set; }
         public DateTime Birthday{ get; set; }
         public string Gender{ get; set; }
