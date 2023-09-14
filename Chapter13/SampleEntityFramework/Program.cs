6e368e087df1a69a9a24239fa06d75e24216836d
諏訪佑哉 <OIBC34011@ojs.ac.jp>
2023-09-14 11:22:50 +0900

    13章演習問題、一部改良

--- Chapter13/SampleEntityFramework/Program.cs
+++ Chapter13/SampleEntityFramework/Program.cs
@@ -14,6 +14,65 @@ namespace SampleEntityFramework {
             //InsertBooks ();
             //AddAuthors ();
 
+            #region データの追加
+            using (var db = new BooksDbContext ()) {
+                var book1 = new Book {
+                    Title = "坊ちゃん",
+                    PublishedYear = 2003,
+                    Author = new Author {
+                        Birthday = new DateTime ( 1867, 2, 9 ),
+                        Gender = "M",
+                        Name = "夏目漱石",
+                    }
+                };
+                db.Books.Add ( book1 );
+                var book2 = new Book {
+                    Title = "人間失格",
+                    PublishedYear = 1990,
+                    Author = new Author {
+                        Birthday = new DateTime ( 1909, 6, 19 ),
+                        Gender = "M",
+                        Name = "太宰治",
+                    }
+                };
+                db.Books.Add ( book2 );
+                db.SaveChanges ();
+
+                var yosano = new Author {
+                    Birthday = new DateTime ( 1878, 12, 7 ),
+                    Gender = "F",
+                    Name = "与謝野晶子",
+                };
+                db.Authors.Add ( yosano );
+
+                var author2 = new Author {
+                    Birthday = new DateTime ( 1896, 8, 27 ),
+                    Gender = "M",
+                    Name = "宮沢賢治",
+                };
+                db.Authors.Add ( author2 );
+                db.SaveChanges ();
+
+                var author1 = db.Authors.First ( a => a.Name == "与謝野晶子" );
+                var book3 = new Book {
+                    Title = "みだれ髪",
+                    PublishedYear = 2000,
+                    Author = yosano,
+                };
+                db.Books.Add ( book3 );
+
+                var miyazawa = db.Authors.First ( a => a.Name == "宮沢賢治" );
+                var book4 = new Book {
+                    Title = "銀河鉄道の夜",
+                    PublishedYear = 1989,
+                    Author = author2,
+                };
+                db.Books.Add ( book4 );
+
+                db.SaveChanges ();
+            }
+            #endregion
+
 
             Console.WriteLine ( "# 1.1" );
             Exercise1_1 ();
@@ -63,35 +122,35 @@ namespace SampleEntityFramework {
 
 
                 //Book追加
-                var author3 = db.Authors.First ( a => a.Name == "夏目漱石" );
+                var natsume = db.Authors.First ( a => a.Name == "夏目漱石" );
                 var book1 = new Book {
                     Title = "こころ",
                     PublishedYear = 1991,
-                    Author = author3,
+                    Author = natsume,
                 };
                 db.Books.Add ( book1 );
 
-                var author4 = db.Authors.First ( a => a.Name == "川端康成" );
+                var kawabata = db.Authors.First ( a => a.Name == "川端康成" );
                 var book2 = new Book {
                     Title = "伊豆の踊子",
                     PublishedYear = 2003,
-                    Author = author4,
+                    Author = kawabata,
                 };
                 db.Books.Add ( book2 );
 
-                var author5 = db.Authors.First ( a => a.Name == "菊池寛" );
+                var kikuchi = db.Authors.First ( a => a.Name == "菊池寛" );
                 var book3 = new Book {
                     Title = "真珠夫人",
                     PublishedYear = 2002,
-                    Author = author5,
+                    Author = kikuchi,
                 };
                 db.Books.Add ( book3 );
 
-                var author6 = db.Authors.First ( a => a.Name == "宮沢賢治" );
+                var miyazawa = db.Authors.First ( a => a.Name == "宮沢賢治" );
                 var book4 = new Book {
                     Title = "注文の多い料理店",
                     PublishedYear = 2000,
-                    Author = author6,
+                    Author = miyazawa,
                 };
                 db.Books.Add ( book4 );
 
@@ -111,10 +170,9 @@ namespace SampleEntityFramework {
 
         private static void Exercise1_3() {
             using (var db = new BooksDbContext()) {
-                var orderedByTitle = db.Books.OrderByDescending ( b => b.Title.Length ).ToList();//タイトルの長さで並び替え
                 int maxLength = db.Books.Max ( b => b.Title.Length );   //タイトルの長さの最大値
 
-                var longestBooks = orderedByTitle.TakeWhile ( b => b.Title.Length >= maxLength );
+                var longestBooks = db.Books.Where( b => b.Title.Length >= maxLength );
                 foreach (var book in longestBooks) {
                     Console.WriteLine ( $"{book.Title}" );
                 }
@@ -146,13 +204,8 @@ namespace SampleEntityFramework {
                         Console.WriteLine ($"{author.Name}:{book.Title}");
                     }
                     Console.WriteLine ( "=================" );
-
                 }
-
-
             }
-
-
         }
 
         // List 13-5
@@ -179,8 +232,6 @@ namespace SampleEntityFramework {
                 };
                 db.Books.Add ( book2 );
                 db.SaveChanges ();
-                //Console.WriteLine ($"{book1.Id}:{book2.Id}");
-                
             }
         }
 
