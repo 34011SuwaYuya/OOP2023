17d570282863d4d0ea960c69f58c2d427da98565
諏訪佑哉 <OIBC34011@ojs.ac.jp>
2023-09-27 12:09:32 +0900

    ItemData実装

--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -4,14 +4,36 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent ();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead ( tbUrl.Text );
+
+                XDocument xdoc = XDocument.Load ( url );
+                var nodes = xdoc.Root.Descendants ( "item" ).Select(x => new ItemData {
+                    Title = x.Element("title").Value,
+                    //link = x.Element("link").Value,
+                }
+                );
+
+                foreach (var item in nodes) {
+                    lbRssTitle.Items.Add ( item.Title);
+                    //lbRssTitle.Items.Add ( item.link);
+                }
+
+            }
         }
     }
 }
