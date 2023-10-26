fa8e3883c80c5bdd721eaf15807a5ca1e1aee0db
諏訪佑哉 <OIBC34011@ojs.ac.jp>
2023-10-26 12:07:17 +0900

    10月26日進捗

--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -18,8 +18,25 @@ namespace ColorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        Dictionary<int, (int, int, int)> keyValuePairs = new Dictionary<int, (int, int, int)> ();
+
         public MainWindow() {
             InitializeComponent ();
+
         }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            colorArea.Background = new SolidColorBrush ( Color.FromRgb ( byte.Parse ( rValue.Text ), byte.Parse ( gValue.Text ), byte.Parse ( bValue.Text ) ) );
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        
     }
+    //public class ColorValue(){
+    //        public int value;
+
+    //    }
 }
